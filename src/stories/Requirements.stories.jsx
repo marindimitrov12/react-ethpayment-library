c7c5d35d6ab1ffe98a3d563d23083296a5583b8c
marindimitrov12 <marin200041@gmail.com>
2024-03-08 14:13:10 +0200

    Initial commit

---
+++ src/stories/Requirements.stories.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Requirements } from '../components/Requirements';
+
+export default {
+    title: 'Requirements',
+    component: Requirements,
+  };
+
+  export const ExampleStory = () => <Requirements/>;
