91c82dc68e4bb1cb65ca9d3fd641f7973c83513c
marindimitrov12 <marin200041@gmail.com>
2024-03-14 17:15:46 +0200

    add web3.js initial set up

--- src/components/Requirements/EthPaymentComponent.jsx
+++ src/components/Requirements/EthPaymentComponent.jsx
@@ -1,11 +1,41 @@
 import '../../style.css';
 import EthereumLogo from '../../ethereum_logo.png';
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+import Web3 from 'web3';
+
 export const EthPaymentComponent=()=>{
     const [amount, setAmount] = useState('');
     const [recipient, setRecipient] = useState('');
   const [loading, setLoading] = useState(false);
   const [paymentStatus, setPaymentStatus] = useState('');
+  
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [allAccounts, setAllAccounts] = useState([]);
+  console.log(currentAccount);
+  useEffect(() => {
+    const initializeWeb3 = async () => {
+      if (window.ethereum) {
+        try {
+          // Request account access if needed
+          await window.ethereum.enable();
+
+          // Initialize web3
+          const web3 = new Web3(window.ethereum);
+
+          // Get current account
+          const accounts = await web3.eth.getAccounts();
+          setCurrentAccount(accounts[0]);
+          setAllAccounts(accounts);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    };
+
+    initializeWeb3();
+  }, []);
+
+
  return(<> 
   <div className="form-container">
       <h2 className="form-heading">
@@ -13,7 +43,21 @@ export const EthPaymentComponent=()=>{
         EthPayment Checkout
       </h2>
       <img src={EthereumLogo} alt="Ethereum Logo" className="ethereum-logo" />
+      
       <form className="payment-form" onSubmit={e => { e.preventDefault(); handlePayment(); }}>
+      <div className="select-container">
+          <label htmlFor="account" className="form-label">Account :</label>
+          <select className="form-input" onChange={(e)=>{
+            setCurrentAccount(e.target.value);
+           
+          }}>
+            {allAccounts.map((account,index)=>(
+              <option key={index} value={account}>{account}</option>
+            ))}
+             
+          </select>
+        
+        </div>
         <div className="form-group">
           <label htmlFor="amount" className="form-label">Amount (ETH):</label>
           <input
