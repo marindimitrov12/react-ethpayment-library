306a17e8ef88ee6158d340934e2077620c8fefbc
marindimitrov12 <marin200041@gmail.com>
2024-03-27 13:02:49 +0200

    refactoring to events

--- src/components/Requirements/PaymentEventComponent .jsx
+++ src/components/Requirements/PaymentEventComponent .jsx
@@ -8,35 +8,7 @@ export const PaymentEventComponent = (props) => {
     const [loading, setLoading] = useState(true);
     const [transactions, setTransactions] = useState([]);
 
-    useEffect(() => {
-        const initializeWeb3 = async () => {
-            try {
-                if (window.ethereum) {
-                    await window.ethereum.request({ method: 'eth_requestAccounts' });
-                    const initializedWeb3 = new Web3(window.ethereum);
-                    const contract = new initializedWeb3.eth.Contract(contractABI.abi, props.contractAddress);
-                    const accounts = await initializedWeb3.eth.getAccounts();
-                    const result = await contract.methods.getTotalPayments(props.userAddress).call({ from: props.userAddress });
-                    const amounts = result[0];
-                    const recipients = result[1];
-                    const timestamps = result[2];
-
-                    setTransactions(amounts.map((amount, index) => ({
-                        recipient: recipients[index],
-                        amount: initializedWeb3.utils.fromWei(amount, 'ether'),
-                        timestamp: timestamps[index]
-                    })));
-                    setLoading(false);
-                } else {
-                    console.error("MetaMask extension not detected");
-                }
-            } catch (error) {
-                console.error('Error:', error);
-            }
-        };
-
-        initializeWeb3();
-    }, [props.contractAddress, props.userAddress]);
+   
     useEffect(()=>{
       const initializedWeb3=async()=>{
         try{
@@ -47,19 +19,26 @@ export const PaymentEventComponent = (props) => {
               
               console.log(contract.events);
               try {
-                // Get the latest block number
+               
                 const latestBlock = await initializedWeb3.eth.getBlockNumber();
         
-                // Fetch past events
+                
                 const events = await contract.getPastEvents('PaymentReceived', {
                     filter: { _to: props.userAddress},
                     fromBlock: 0,
                     toBlock: latestBlock,
                     
                 });
-        
-                console.log('All past events:', events
-                );
+               
+               
+                setTransactions(events.map((amount, index) => ({
+                  recipient: events[index].returnValues[0] ,
+                  amount: initializedWeb3.utils.fromWei(events[index].returnValues[2] , 'ether'),
+                  timestamp: events[index].returnValues[3] 
+              })));
+              setLoading(false);
+
+                
             } catch (error) {
                 console.error('Error fetching past events:', error);
             }
@@ -72,7 +51,7 @@ export const PaymentEventComponent = (props) => {
       }
       initializedWeb3();
     },[]);
-
+     console.log(transactions);
     return (
         <div className="table-container">
             {loading ? <h1>Loading...</h1> :
